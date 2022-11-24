482a461f1a09fe13a597bca8d2a7e6c9e85793a4
KatieRina <eashchukina@yandex.ru>
2022-11-25 02:15:44 +0300

    третья задача

---
+++ HW68/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте значение N. Напишите программу,
+//которая выведет все натуральные числа в промежутке от 1 до N.
+// Console.Clear();
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine());
+// PrintNumbers(1,n);
+// void PrintNumbers(int start, int end)
+// {
+// Console.Write($"{start} ");
+// if (start == end) return;
+// PrintNumbers(start + 1, end);
+// }
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akker(int a, int b)
+{
+        if (a == 0) return b + 1;
+        else if (b == 0) return Akker(a - 1, 1);
+             else return Akker(a - 1, Akker(a, b - 1));
+}
+
+Console.Clear();
+Console.Write("Введите m и n через пробел: ");
+string? ins = Console.ReadLine();
+string[] mn = ins.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int m = Convert.ToInt32(mn [0]);
+int n = Convert.ToInt32(mn [1]);
+int akk = Akker(m, n);
+Console.WriteLine($"A({m}, {n}) = {akk}");
+
+
+
+
+
