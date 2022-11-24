142db89bca360e1afe31750e1cc2aebf63d72869
KatieRina <eashchukina@yandex.ru>
2022-11-25 01:13:26 +0300

    вторая задача

---
+++ HW66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SummNumbers(int start, int end, int summ)
+{
+    summ += start;
+    if (start == end)
+    {
+        Console.WriteLine(summ);
+        return;
+    }
+    SummNumbers(start + 1, end, summ);
+}
+
+Console.Clear();
+Console.Write("Введите M и N через пробел: ");
+string? ins = Console.ReadLine();
+string[] mn = ins.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int m = Convert.ToInt32(mn [0]);
+int n = Convert.ToInt32(mn [1]);
+while (m > n)
+{
+    Console.Write("M должно быть меньше N. Введите ещё раз: ");
+    ins = Console.ReadLine();
+    mn = ins.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    m = Convert.ToInt32(mn[0]);
+    n = Convert.ToInt32(mn[1]);
+}
+int startSumm = 0;
+SummNumbers(m, n, startSumm);
